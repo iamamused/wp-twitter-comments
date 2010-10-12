2acf07a7dc7b677cf1c1dc3993f1cedc0351d9c7
Jeffrey Sambells <github@tropicalpixels.com>
2010-10-11 18:49:20 -0700

    Updated license and comment block.

--- twitter-comments.php
+++ twitter-comments.php
@@ -1,15 +1,28 @@
 <?php
 /*
 Plugin Name: Twitter Comments
+Plugin URI: http://github.com/iamamused/twitter-comments
+Description: Integrates Twitter with the WordPress commentig system. Commentors must authorize the blog as an app before post is created and tweeted.
 Version: 1.0
-Description: Integrates twitter with the wordpress commentig system. Commentors must authenticate with twitter before post is created.
-Contributors: Jeffrey Sambells
 Author: Jeffrey Sambells
-Author URI: http://jeffreysambells.com/
-Plugin URI: 
-Donate Link: 
-Update Server: 
-Disclaimer: No warranty is provided. PHP 5 required.
+Author URI: http://jeffreysambells.com
+License: GPL2
+
+	Copyright 2010  Jeffrey Sambells  (email : github@tropicalpixels.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 Requires at least: 2.7
 */
 
